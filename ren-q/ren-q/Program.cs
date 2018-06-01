5929dbe9beccc657bb22d485c7c8d1e6a4e047da
rkillaars <ron.killaars@ren-it.net>
2018-06-01 11:22:45 +0800

    fixed the port setting

--- ren-q/ren-q/Program.cs
+++ ren-q/ren-q/Program.cs
@@ -20,6 +20,7 @@ namespace ren_q
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("http://localhost:8888")
                 .Build();
     }
 }
